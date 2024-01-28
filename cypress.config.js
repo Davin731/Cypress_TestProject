160c8751626613342725aa80753577bf406d3840
Davinderpal <31davinderpal@gmail.com>
2024-01-28 10:22:12 -0500

    Removed reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.google.com",
-    reporter: 'cypress-mochawesome-reporter',
     defaultCommandTimeout: 3000,
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      
     },
   },
 });
