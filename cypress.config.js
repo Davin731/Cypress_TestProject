b2420c02a9f17bad769b5786e04c5bcf0a92ac6d
Davinderpal <31davinderpal@gmail.com>
2024-01-28 10:32:18 -0500

    Added further new files

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.google.com",
     defaultCommandTimeout: 3000,
+    reporter: 'cypress-mochawesome-reporter',
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
